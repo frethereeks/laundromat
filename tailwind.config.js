5fd822677f3b9f0efc9e62b4035f4015d93b43c8
Frederick Jones <Frederickjones@gmail.com>
2024-06-05 12:46:54 +0100

    Create laundromat website. Add images in assets folder. Create headerLInks in the data folder and use react-router-dom to handle the routing

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        backdrop: "#fsfaff",
+        // dark: "#090808",
+        dark: "#134264",
+        secondary: "#f16b86",
+        yell: "#f9b940",
+      },
+    },
+  },
+  plugins: [],
+};
